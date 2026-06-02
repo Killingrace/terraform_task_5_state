a03c2ff601f831d70b0888de98b0f384f97e6a16
Dmytro <dima.shutko5@gmail.com>
2026-06-02 15:35:18 +0200

    test

---
+++ main.tf
@@ -0,0 +1,27 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "4.75.0"
+    }
+  }
+  backend "azurerm" {
+    storage_account_name = "myuniquesan2345"
+    resource_group_name  = "tfstate"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+    use_oidc             = true
+  }
+}
+
+provider "azurerm" {
+  features {
+
+  }
+}
+
+
+resource "azurerm_resource_group" "test_rg" {
+  name     = "testhowworks"
+  location = "Poland Central"
+}
